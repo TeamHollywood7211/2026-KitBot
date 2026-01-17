36e52bb36afe4c72104f09d22c3089147646c103
Nicholas Doty <n.faustdoty@gmail.com>
2026-01-16 19:39:04 -0500

    Lillian baller paths

--- src/main/java/frc/robot/ShootSequence.java
+++ src/main/java/frc/robot/ShootSequence.java
@@ -8,17 +8,17 @@ import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShootSequence extends ParallelCommandGroup {
     public ShootSequence(TurretSubsystem turret, ShooterSubsystem shooter) {
-        addCommands(
-            // 1. Always keep the shooter spinning at target RPM
-            shooter.runOnce(() -> shooter.setShooterVelocity(3500)),
+        // addCommands(
+        //     // 1. Always keep the shooter spinning at target RPM
+        //     shooter.runOnce(() -> shooter.setShooterVelocity(3500)),
 
-            // 2. Run the alignment and feeding sequence in parallel
-            new SequentialCommandGroup(
-                // Wait for the drivetrain to settle at the end of the path
-                new WaitCommand(0.5), 
-                // Run the actual shooting logic until hopper is empty
-                new ShootCommand(turret, shooter)
-            )
-        );
+        //     // 2. Run the alignment and feeding sequence in parallel
+        //     new SequentialCommandGroup(
+        //         // Wait for the drivetrain to settle at the end of the path
+        //         new WaitCommand(0.5), 
+        //         // Run the actual shooting logic until hopper is empty
+        //         new ShootCommand(turret, shooter)
+        //     )
+        // );
     }
 }
